e960143bde4e3cd7ce5609642a98ebfbb69fa127
Firas <firashijazi01@gmail.com>
2022-12-16 15:00:25 -0500

    deploy to s3

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1>Welcome to Grocery Application</h1>
+    <h1>Welcome to Grocery Application New</h1>
     </div>
   );
 }
