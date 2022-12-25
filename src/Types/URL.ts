136f23256a2825a932f57fa0b5682c331b28ee38
Firas <firashijazi01@gmail.com>
2022-12-24 20:34:27 -0500

    Version 2.0 JWT DevOps React SpringBoot

--- src/Types/URL.ts
+++ src/Types/URL.ts
@@ -1,3 +1,3 @@
-export const url:string = 'http://34.201.165.127:8500';
+export const remoteUrl:string = 'http://34.201.165.127:8500';
 
-export const remoteUrl:string= 'http://localhost:8500';
+export const url:string= 'http://localhost:8500';
